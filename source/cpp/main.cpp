ffabfb9dfd1e70164118b8d24ee308e50b513d38
Euler <119917246+Bird-of-Hermes@users.noreply.github.com>
2024-07-28 05:15:54 -0300

    Update main.cpp

--- source/cpp/main.cpp
+++ source/cpp/main.cpp
@@ -1,8 +1,4 @@
 #include "../Raytracer.h"
-#include <string>
-#include <execution>
-#include <algorithm>
-#include <thread>
 
 int WinMain([[maybe_unused]] _In_ HINSTANCE hInstance, [[maybe_unused]] _In_opt_ HINSTANCE hPrevInstance, [[maybe_unused]] _In_ LPSTR lpCmdLine, [[maybe_unused]] _In_ int nShowCmd)
 {
